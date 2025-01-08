a1d1b33068aca9a28fada470145f2aadb089bfde
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-01-08 19:28:09 +0530

    Time: 2 ms (58.7%), Space: 46.2 MB (50.06%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,40 +2,42 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int [] arr = new int[n1 + n2];
-
+        int len = n1 + n2;
+        int count = 0;
+        double median = 0;
         int i = 0;
         int j = 0;
-        int k = 0;
+        int num1 = 0;
+        int num2 = 0;
 
         while(j < n2 && i < n1) {
             if(nums1[i] < nums2[j]) {
-                arr[k] = nums1[i];
+                if(count == len/2 - 1) num1 = nums1[i];
+                if(count == len/2) num2 = nums1[i];
                 i++;
             } else {
-                arr[k] = nums2[j];
+                if(count == len/2 - 1) num1 = nums2[j];
+                if(count == len/2) num2 = nums2[j];
                 j++;
             }
-            k++;
+            count++;
         }
         while(j < n2) {
-            arr[k] = nums2[j];
+            if(count == len/2 - 1) num1 = nums2[j];
+            if(count == len/2) num2 = nums2[j];
+            count++;
             j++;
-            k++;
         }
         while(i < n1) {
-            arr[k] = nums1[i];
+            if(count == len/2 - 1) num1 = nums1[i];
+            if(count == len/2) num2 = nums1[i];
+            count++;
             i++;
-            k++;
         }
-
-        i = 0;
-        double median = 0;
-        int len = arr.length;
         if(len % 2 == 0) {
-            median = ((double)arr[len / 2] + (double) arr[(len/2) - 1]) / (double)2;
+            median = ((double)num1 + (double) num2) / (double)2;
         } else {
-            median = (double)(arr[len / 2]);
+            median = (double)(num2);
         }
 
         return median;
