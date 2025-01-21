1e7f505a2fe0389a84901e8d5cbb00ea09aed603
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-01-21 19:22:32 +0530

    Time: 7 ms (6.64%), Space: 42.9 MB (5.99%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.java
@@ -1,24 +1,29 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
         int len = word.length();
-        char [] wrd = word.toCharArray();
-        if(len == 1) return word;
+        String str = "";
         int index = -1;
+        Stack<Character> stk = new Stack<>();
         for(int i = 0; i < len; i++) {
-            if(ch == wrd[i]) {
-                index = i ;
+            if(word.charAt(i) == ch) {
+                stk.push(ch);
+                index = i+1;
                 break;
             }
+            stk.push(word.charAt(i));
         }
-        StringBuilder str = new StringBuilder();
+        if(stk.isEmpty() || index == -1) return word;
 
-        for(int i = index; i >= 0; i--) {
-            str.append(wrd[i]);
+        while(!stk.isEmpty()) {
+            char c = stk.pop();
+            str += c;
         }
-        for(int i = index + 1; i < len; i++) {
-            str.append(wrd[i]);
+        if(index == len) return str;
+        for(int i = index; i < len; i++) {
+            char c = word.charAt(i);
+            str += c;
         }
 
-        return str.toString();
+        return str;
     }
 }
