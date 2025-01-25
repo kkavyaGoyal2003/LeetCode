ab37419ecc5653d17c2b5bc0e666a71fc9e0a934
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-01-25 10:55:15 +0530

    Time: 19 ms (24.15%), Space: 46 MB (87.53%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,19 +1,44 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] sol = new int[n];
+        List<Integer> sol = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
 
-        for(int i = 0; i < n; i++) {
-            boolean flag = true;
-            for(int j = i + 1; j < i+n; j++) {
-                if(nums[j % n] > nums[i] ) {
-                    sol[i] = nums[j % n];
-                    flag = false;
-                    break;
+        for(int i = 2*n-1; i >= 0; i--){
+            if(i < n) {
+                if(stk.isEmpty()) {
+                    sol.add(-1);
+                    stk.push(nums[i]);
+                } else if(stk.peek() > nums[i]) {
+                    sol.add(stk.peek());
+                    stk.push(nums[i]);
+                } else {
+                    while(!stk.isEmpty() && stk.peek() <= nums[i]) stk.pop();
+                    if(stk.isEmpty()) {
+                        sol.add(-1);
+                    } else {
+                        sol.add(stk.peek());
+                    }
+                    stk.push(nums[i]);
+                }
+            } else {
+                if(stk.isEmpty()) {
+                    stk.push(nums[i % n]);
+                } else if(stk.peek() > nums[i %n]) {
+                    stk.push(nums[i % n]);
+                } else {
+                    while(!stk.isEmpty() && stk.peek() <= nums[i % n]) stk.pop();
+                    stk.push(nums[i % n]);
                 }
             }
-            if(flag) sol[i] = -1;
         }
-        return sol;
+
+        int i = 0;
+        for(int j = sol.size() - 1 ; j >= 0; j--) {
+            nums[i] = sol.get(j);
+            i++;
+        }
+
+        return nums;
     }
 }
