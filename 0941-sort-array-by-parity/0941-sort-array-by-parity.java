09f62d03bfc92dc982f44525bda978754eb6cf35
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-01-29 18:02:27 +0530

    Time: 1 ms (49.05%), Space: 44.8 MB (88.45%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,25 +1,24 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums;
-        
-        ArrayList<Integer> even = new ArrayList<>();
-        ArrayList<Integer> odd = new ArrayList<>();
+        int i = 0;
+        int j = n - 1;
 
-        for(int i = 0; i < n; i++) {
-            if(nums[i] % 2 == 0) {
-                even.add(nums[i]);
+        while(i < j) {
+            if(nums[i]  % 2 == 0 && nums[j] % 2 == 1) {
+                i++;
+                j--;
+            } else if(nums[i]  % 2 == 0 && nums[j] % 2 == 0) {
+                i++;
+            } else if(nums[i]  % 2 == 1 && nums[j] % 2 == 1) {
+                j--;
             } else {
-                odd.add(nums[i]);
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
             }
-        }
-
-        for(int i = 0; i < even.size(); i++) {
-            nums[i] = even.get(i);
-        }
-
-        for(int i = 0; i < odd.size(); i++) {
-            nums[n-1-i] = odd.get(i);
         }
 
         return nums;
