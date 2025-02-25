106b434bc572f2a1231228b04336c7af22b0800a
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-02-25 15:06:22 +0530

    Time: 53 ms (30.56%), Space: 55.5 MB (24.87%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -12,9 +12,9 @@ class Solution {
                 map.put(fruits[i], map.get(fruits[i]) - 1);
                 if(map.get(fruits[i]) == 0) map.remove(fruits[i]);
                 i++;
-            }
-            
-            totalFruits = Math.max(totalFruits, j-i+1);
+            } else {
+                totalFruits = Math.max(totalFruits, j-i+1);
+            }         
             j++;
         }
         return totalFruits;
