f5c5b3c7dc6bf83f93fcab746534039e70f29e68
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-05-24 19:05:34 +0530

    Time: 0 ms (100%), Space: 40.9 MB (76.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) return nums[0];
+        int[] arr = new int[n];
+        Arrays.fill(arr, -1);
+        
+        return stealMoney(nums, 0, arr);
+    }
+    public int stealMoney(int[] nums, int index, int[] arr) {
+        if(index >= nums.length) return 0;
+        if(arr[index] != -1) return arr[index];
 
-        int prev = nums[0];
-        int curr = Math.max(nums[0], nums[1]);
-
-        for(int i = 2; i < n; i++) {
-            int next = curr;
-            curr = Math.max(curr, nums[i] + prev);
-            prev = next;
-        }
-        return curr;
+        int case1 = stealMoney(nums, index + 1, arr);
+        int case2 = nums[index] + stealMoney(nums, index + 2, arr);
+        arr[index] = Math.max(case1, case2);
+        return arr[index];
     }
 }
