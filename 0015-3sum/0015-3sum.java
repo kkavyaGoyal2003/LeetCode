6ff6cb7d43ee22d1958ccbde65c154929743dbc3
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-05-29 12:22:22 +0530

    Time: 2302 ms (5.01%), Space: 53.5 MB (9.05%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> sol = new ArrayList<>();
+        HashSet<List<Integer>> sol = new HashSet<>();
         int n = nums.length;
 
-        Arrays.sort(nums);
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > 0) break;
-            if(i != 0 && nums[i] == nums[i-1]) continue;
-
-            int low = i+1;
-            int high = n-1;
-            int target = ( nums[i] * -1);
-            while(low < high) {
-                if(nums[low] + nums[high] == target) {
-                    List<Integer> temp = Arrays.asList(nums[i], nums[low], nums[high]);
+        for(int i = 0; i < n - 2; i++) {
+            int target  = -1 * nums[i];
+            HashSet<Integer> set = new HashSet<>();
+            for(int j = i+1; j <n; j++) {
+                int comp = target - nums[j];
+                if(set.contains(comp)) {
+                    List<Integer> temp = Arrays.asList(nums[i], nums[j], comp);
+                    Collections.sort(temp);
                     sol.add(temp);
-                    while(low < high && nums[low] == nums[low + 1]) low++;
-                    while(low < high & nums[high] == nums[high - 1]) high--;
-                    low++;
-                    high--;
-                } else if(nums[low] + nums[high] < target) {
-                    low++;
                 } else {
-                    high--;
+                    set.add(nums[j]);
                 }
             }
         }
 
-        return sol;
+        return new ArrayList<>(sol);
     }
 }
