2ab09e84299f486e4e186485b30bc1e1f07fa76f
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-06-02 22:01:05 +0530

    Time: 2 ms (94.41%), Space: 42.3 MB (10.77%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,31 +1,27 @@
 class Solution {
-    public int calPoints(String[] operations) {
-        Stack<Integer> score = new Stack<>();
-        
-        for(String op : operations) {
-            if(isInteger(op)) {
-                score.push(Integer.parseInt(op));
-            } else if(op.equals("+")) {
-                int top1 = score.pop();
-                int top2 = score.pop();
-                score.push(top2);
-                score.push(top1);
-                score.push(top2 + top1);
-            } else if(op.equals("D")){
-                score.push(2 * score.peek());
-            } else if (op.equals("C")) {
-                if(!score.isEmpty())score.pop();
+    public int calPoints(String[] operations) {        
+        Stack<Integer> stk = new Stack<>();
+
+        for(String s : operations) {
+            if(s.equals("C")) {
+                if(!stk.isEmpty()) stk.pop();
+            } else if(s.equals("D")) {
+                stk.push(2*stk.peek());
+            } else if(s.equals("+")) {
+                int top1 = stk.pop();
+                int top2 = stk.pop();
+                stk.push(top2);
+                stk.push(top1);
+                stk.push(top2 + top1);
+            } else {
+                stk.push(Integer.parseInt(s));
             }
         }
 
-        int totalScore = 0;
-        while(!score.isEmpty()) {
-            totalScore += score.pop();
+        int total = 0;
+        while(!stk.isEmpty()) {
+            total += stk.pop();
         }
-
-        return totalScore;
-    }
-    public static boolean isInteger(String str) {
-        return str.matches("-?\\d+");
+        return total;
     }
 }
