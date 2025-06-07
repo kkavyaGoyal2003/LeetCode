5c02cc732e308473d3ecb4c27f20acc0706e822f
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-06-07 16:06:54 +0530

    Time: 2 ms (86.36%), Space: 48 MB (89.45%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,53 +10,32 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null) {
-            return ;
-        }
-        ListNode mid = midNode(head);
-        ListNode second = reverse(mid.next);
-        mid.next = null;
-
-        reorderLinkedList(head, second);
-    }
-    public ListNode midNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null) {
+        while(fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        return slow;
-    }
-
-    public ListNode reverse(ListNode slow) {
-        ListNode prev = null;
-        ListNode temp = slow;
-
-        while(temp != null) {
-            ListNode node = temp.next;
-            temp.next = prev;
-            prev = temp;
-            temp = node;
+        fast = reverse(slow.next);
+        slow.next = null;
+        slow = head;
+        while(fast != null) {
+            ListNode temp = slow.next;
+            slow.next = fast;
+            fast = fast.next;
+            slow.next.next = temp;
+            slow = temp;
         }
-
+    }
+    public ListNode reverse(ListNode curr) {
+        ListNode prev = null;
+        while(curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
         return prev;
     }
-
-    public void reorderLinkedList(ListNode head, ListNode mid) {
-        ListNode first = head;
-        ListNode second = mid;
-
-        while(second != null) {
-            ListNode temp1 = first.next;
-            ListNode temp2 = second.next;
-            first.next = second;
-            second.next = temp1;
-            first = temp1;
-            second = temp2;
-        }
-    }
-
 }
