849e6dfd755569d3beabd68eb78e98e16aeb4143
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-06-17 22:03:55 +0530

    Time: 3 ms (22.97%), Space: 44.6 MB (81.12%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    List<List<Integer>> sol = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        countPairs(candidates, target, 0, new ArrayList<Integer>());
+        return sol;
+    }
+    public void countPairs(int[] candidates, int target, int ind , List<Integer> path) {
+        if(target == 0) {
+            sol.add(new ArrayList<>(path));
+            return;
+        }
+        if(target < 0 || ind == candidates.length) return;
+
+        
+
+        path.add(candidates[ind]);
+        countPairs(candidates, target - candidates[ind], ind, path);
+        path.remove(path.size() - 1);
+        countPairs(candidates, target, ind+1, path);
+        
+    }
+}
