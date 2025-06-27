070221cc5a7fef374fff714115f1e716666922b0
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-06-27 20:58:48 +0530

    Time: 1 ms (100%), Space: 46 MB (84.45%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -2,68 +2,36 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
+        int total = n1 + n2;
 
-        if(n1 == 0) {
-            if(n2 % 2 != 0) {
-                return (double)(nums2[n2 / 2]);
-            } else {
-                return (double)((nums2[n2 / 2] + nums2[(n2 / 2 ) - 1]) / 2.0);
-            }
-        }
-        if(n2 == 0) {
-            if(n1 % 2 != 0) {
-                return (double)(nums1[n1 / 2]);
-            } else {
-                return (double)((nums1[n1 / 2] + nums1[n1 / 2 - 1]) / 2.0);
-            }
-        }
-
-        if(n1 > n2) {
+        if (n1 > n2) {
             return findMedianSortedArrays(nums2, nums1);
         }
-        int low = 0;
-        int high = n1;
 
-        int ele = (n1 + n2 + 1) / 2;
-        
-        while(low <= high) {
-            int mid1 = low + ((high - low) / 2);
-            int mid2 =  ele - mid1;
+        int low = 0, high = n1;
 
-            int l1 = Integer.MIN_VALUE;
-            int l2 = Integer.MIN_VALUE;
-            int r1 = Integer.MAX_VALUE;
-            int r2 = Integer.MAX_VALUE;
-            
-            if (mid1-1 >= 0) {
-                l1 = nums1[mid1 -1];
-            }
-            if(mid2-1 >= 0) {
-                l2 = nums2[mid2-1];
-            }
-            if(mid1 < n1) {
-                r1 = nums1[mid1];
-            }
-            if(mid2 < n2) {
-                r2 = nums2[mid2];
-            }
+        while (low <= high) {
+            int partX = low + (high - low) / 2;
+            int partY = (total + 1) / 2 - partX;
 
-            if(l1 <= r2 &&  l2 <= r1) {
-                if((n1+n2) % 2 != 0) {
-                    double max = (double) Math.max(l1, l2);
-                    return max;
+            int x1 = partX == 0 ? Integer.MIN_VALUE : nums1[partX - 1];
+            int y1 = partX == n1 ? Integer.MAX_VALUE : nums1[partX];
+            int x2 = partY == 0 ? Integer.MIN_VALUE : nums2[partY - 1];
+            int y2 = partY == n2 ? Integer.MAX_VALUE : nums2[partY];
+
+            if (x1 <= y2 && x2 <= y1) {
+                if (total % 2 == 0) {
+                    return (Math.max(x1, x2) + Math.min(y1, y2)) / 2.0;
                 } else {
-                    int total = Math.max(l1, l2) + Math.min(r1, r2);
-                    double median = (double)(total) / (double)(2);
-                    return median;
+                    return Math.max(x1, x2);
                 }
-            } else if (l1 > r2) {
-                high = mid1 - 1;
+            } else if (x1 > y2) {
+                high = partX - 1;
             } else {
-                low = mid1 + 1;
+                low = partX + 1;
             }
         }
 
-        return 0;    
+        return 0.0;
     }
 }
