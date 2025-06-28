700ea298319df3d0c645e57cb0f6f6e6649d016e
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-06-28 16:15:43 +0530

    Time: 0 ms (100%), Space: 48.1 MB (28.78%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,20 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        int low = 0; 
-        int high = n - 1;
-        int papers = 0;
+        int low = 0;
+        int high = n-1;
 
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            int count = 0;
-            if(citations[mid] >= n - mid) {
-                papers = n - mid;
+            if(citations[mid] == n-mid) return n-mid;
+
+            if(citations[mid] < n-mid) {
+                low = mid + 1;
+            }else {
                 high = mid - 1;
-            } else {
-                low  = mid + 1;
             }
         }
-        return papers;
+
+        return n-low;
     }
 }
