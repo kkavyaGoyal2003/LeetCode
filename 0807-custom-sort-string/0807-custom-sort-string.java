8153488c21a04b5a92c73ed9c1b1f1d016a6dac1
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-07-01 17:44:44 +0530

    Time: 2 ms (66.79%), Space: 41.6 MB (77.24%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.java
+++ 0807-custom-sort-string/0807-custom-sort-string.java
@@ -1,34 +1,28 @@
 class Solution {
     public String customSortString(String order, String s) {
-        HashMap<Integer, Character> mapO = new HashMap<>();
-        HashMap<Character, Integer> mapS = new HashMap<>();
-
-        for(int i = 0; i < order.length(); i++) {
-            mapO.put(i, order.charAt(i));
-        }
-
+        HashMap<Character, Integer> map = new HashMap<>();
         for(char c : s.toCharArray()) {
-            mapS.put(c, mapS.getOrDefault(c, 0) + 1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         StringBuilder ans = new StringBuilder();
-
         for(int i = 0; i < order.length(); i++) {
-            char c = mapO.get(i);
-            if(mapS.containsKey(c)) {
-                for(int j = 0 ; j < mapS.get(c); j++) {
+            char c = order.charAt(i);
+            if(map.containsKey(c)) {
+                for(int j = 0 ; j < map.get(c); j++) {
                     ans.append(c);
                 }
-                mapS.remove(c);
+                map.remove(c);
             }
         }
 
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(mapS.containsKey(c)) {
-                ans.append(c);
-                mapS.put(c, mapS.get(c) - 1);
-                if(mapS.get(c) == 0) mapS.remove(c);
+            if(map.containsKey(c)) {
+                for(int j = 0 ; j < map.get(c); j++) {
+                    ans.append(c);
+                }
+                map.remove(c);
             }
         }
         return ans.toString();
