58e039426e6ec5e1be5e5f4f1843973b71447902
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-07-16 18:13:02 +0530

    Time: 30 ms (14.47%), Space: 46.2 MB (97.1%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,10 +1,40 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
-        int result = 0;
-        for(int i = 0; i < nums.length; i += 2) {
-            result += nums[i];
+        int n = nums.length;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i : nums) {
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        return result;
+
+        int res = 0;
+        boolean flag = true;
+        for(int i = min; i <= max; i++) {
+            if(!map.containsKey(i)) continue;
+            if(!flag) {
+                map.put(i, map.get(i) - 1);
+                flag = !flag;
+            }
+            int freq = map.get(i);
+            // while(freq > 0) {
+            //     if(flag) res += i;
+            //     freq--;
+            //     flag = !flag;
+            // }
+            if(freq % 2 == 0) {
+                res +=  i*(freq/2);
+                flag = true;
+            } else {
+                
+                res += i*(freq/2);
+                res += i;
+                flag = false;
+            }
+        }
+        return res;
     }
 }
