2a4ad2422ad3e472696b49fad1561cf810e44734
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-07-22 08:43:56 +0530

    Time: 9 ms (93.8%), Space: 47.1 MB (6.49%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,44 +1,18 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        List<Integer> sol = new ArrayList<>();
+        int[] sol = new int[n];
+        Arrays.fill(sol, -1);
+
         Stack<Integer> stk = new Stack<>();
-
-        for(int i = 2*n-1; i >= 0; i--){
-            if(i < n) {
-                if(stk.isEmpty()) {
-                    sol.add(-1);
-                    stk.push(nums[i]);
-                } else if(stk.peek() > nums[i]) {
-                    sol.add(stk.peek());
-                    stk.push(nums[i]);
-                } else {
-                    while(!stk.isEmpty() && stk.peek() <= nums[i]) stk.pop();
-                    if(stk.isEmpty()) {
-                        sol.add(-1);
-                    } else {
-                        sol.add(stk.peek());
-                    }
-                    stk.push(nums[i]);
-                }
-            } else {
-                if(stk.isEmpty()) {
-                    stk.push(nums[i % n]);
-                } else if(stk.peek() > nums[i %n]) {
-                    stk.push(nums[i % n]);
-                } else {
-                    while(!stk.isEmpty() && stk.peek() <= nums[i % n]) stk.pop();
-                    stk.push(nums[i % n]);
-                }
+        for(int i = 0; i < 2*n; i++) {
+            while(!stk.isEmpty() && nums[stk.peek()] < nums[i%n]) {
+                int pop = stk.pop();
+                sol[pop] = nums[i%n];
             }
+            if(i < n) stk.push(i);
         }
 
-        int i = 0;
-        for(int j = sol.size() - 1 ; j >= 0; j--) {
-            nums[i] = sol.get(j);
-            i++;
-        }
-
-        return nums;
+        return sol;
     }
 }
