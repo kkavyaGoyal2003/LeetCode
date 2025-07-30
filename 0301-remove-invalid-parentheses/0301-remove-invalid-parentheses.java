4d1845328cf2b799af4431ac80ea84de47b3f08b
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-07-30 22:29:10 +0530

    Time: 83 ms (42.13%), Space: 45.7 MB (36.14%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -1,34 +1,40 @@
 class Solution {
     public List<String> removeInvalidParentheses(String s) {
+        if(s.length() == 0) return new ArrayList<>();
         List<String> sol = new ArrayList<>();
         HashSet<String> set = new HashSet<>();
         Queue<String> que = new LinkedList<>();
+        this.flag = false;
+        this.max = 0;
 
-        que.offer(s);
-        boolean flag = false;
-        while(!que.isEmpty()) {
-            int size = que.size();
-
-            for(int i = 0; i < size; i++) {
-                String str = que.poll();
-                if(isValid(str)) {
-                    sol.add(str);
-                    flag = true;
-                }
-                if(flag) continue;
-                for(int j = 0; j < str.length(); j++) {
-                    if(s.charAt(j) != '(' && s.charAt(j) != ')') continue;
-                    String sub = str.substring(0,j) + str.substring(j+1);
-                    if(!set.contains(sub)){
-                        que.offer(sub);
-                        set.add(sub);
-                    }
-                }
-            }
-            if(flag) break;
-        }
+        dfs(s, set, sol);
+        if(sol.size() == 0) sol.add("");
 
         return sol;
+
+    }
+    private boolean flag;
+    private int max;
+    private void dfs(String s, HashSet<String> set, List<String> sol) {
+
+        if(s.length() < max) return;
+        set.add(s);
+        if(isValid(s)) {
+            flag = true;
+            if (s.length() > max) {
+                max = s.length();
+                sol.clear();
+            }
+            sol.add(s);
+        } else {
+            for(int j = 0; j < s.length(); j++) {
+                if(s.charAt(j) != '(' && s.charAt(j) != ')') continue;
+                String sub = s.substring(0,j) + s.substring(j+1);
+                if(!set.contains(sub)){
+                    dfs(sub, set, sol);
+                }
+            }
+        }
     }
     private boolean isValid(String s) {
         int count = 0;
