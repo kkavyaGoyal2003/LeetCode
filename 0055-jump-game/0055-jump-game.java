6887af05dc72e40fefc2ad4368da5243b9bdcf96
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-08-10 12:16:10 +0530

    Time: 1 ms (99.84%), Space: 45.8 MB (55.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,19 +3,13 @@ class Solution {
         int n = nums.length;
         if(n == 1) return true;
         if(nums[0] == 0) return false;
-        HashSet<Integer> dp = new HashSet<>();
-        return dfs(nums, 0, dp);
-        
-    }
-    private boolean dfs(int[] nums, int curr, HashSet<Integer> dp) {
-        if(curr == nums.length -1 ) return true ;
-        if(dp.contains(curr)) return false;
-
-        for(int i = 1; i <= nums[curr]; i++) {
-
-            if(dfs(nums, curr+i, dp)) return true;
+        int target = n-1;
+        for(int i = n-2; i>= 0; i--) {
+            if(nums[i] + i >=  target) {
+                target = i;
+            }
         }
-        dp.add(curr);
-        return false;
+        return target == 0;
     }
+    
 }
