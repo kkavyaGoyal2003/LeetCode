0cb514db8dcc9cdb0dca5927d471f5d3f396fcee
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-08-22 10:51:45 +0530

    Time: 1 ms (100%), Space: 44.3 MB (89.24%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -2,30 +2,33 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int lens = s.length();
         int lenp = p.length();
-        boolean[] dp = new boolean[lenp+1];
-        dp[0] = true;
 
-        for(int i = 1; i < lenp+1; i++) {
-            if(p.charAt(i-1) == '*') dp[i] = dp[i-1];
+        int pp = 0;
+        int sp = 0;
+        int pstar = -1;
+        int sstar = -1;
+
+        while(sp < lens) {
+            if(pp < lenp && (s.charAt(sp) == p.charAt(pp) || p.charAt(pp) == '?')) {
+                pp++;
+                sp++;
+            } else if(pp < lenp  && p.charAt(pp) == '*') {
+                pstar = pp;
+                sstar = sp;
+                pp++;
+            } else if(pstar == -1) {
+                return false;
+            } else {
+                sstar = sstar + 1;
+                sp = sstar;
+                pp = pstar + 1; 
+            }
+        }
+        while(pp < p.length()){
+            if(p.charAt(pp) != '*') return false;
+            pp++;
         }
 
-        for(int i = 1; i < lens+1; i++) {
-            boolean digUp = dp[0];
-            dp[0] = false;
-            for(int j = 1; j< lenp+1; j++) {
-                boolean temp = dp[j];
-                if(p.charAt(j-1) == '*'){
-                    dp[j] = dp[j-1] || dp[j];
-                } else {
-                    if(p.charAt(j-1) == '?' || p.charAt(j-1) == s.charAt(i-1)) {
-                        dp[j] = digUp;
-                    } else {
-                        dp[j] = false;
-                    }
-                }
-                digUp = temp;
-            }  
-        }
-        return dp[lenp];
+        return true;
     }
 }
