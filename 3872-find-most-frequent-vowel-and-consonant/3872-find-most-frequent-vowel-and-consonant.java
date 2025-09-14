f9c4c68203b5b271a823932670bb6cf57fd00aff
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-09-14 23:25:21 +0530

    Time: 4 ms (40.14%), Space: 42.3 MB (92.08%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -3,27 +3,17 @@ class Solution {
         HashMap<Character, Integer> vowel = new HashMap<>();
         HashMap<Character, Integer> consot = new HashMap<>();
 
+        int max1 = 0;
+        int max2 = 0;
         for(char c : s.toCharArray()) {
             if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
                 vowel.put(c, vowel.getOrDefault(c, 0) + 1);
+                max1 = Math.max(max1, vowel.get(c));
             } else {
                 consot.put(c, consot.getOrDefault(c, 0) + 1);
+                max2 = Math.max(max2, consot.get(c));
             }
         }
-
-        int v = 0;
-        int c = 0;
-        
-        for(Map.Entry<Character, Integer> ele : vowel.entrySet()) {
-            int num = ele.getValue();
-            v = Math.max(v, num);
-        }
-
-        for(Map.Entry<Character, Integer> ele : consot.entrySet()) {
-            int num = ele.getValue();
-            c = Math.max(c, num);
-        }  
-
-        return v+c;     
+        return max1+max2;    
     }
 }
