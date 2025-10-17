2b062b9501b281ac34d3ccb775fa7392c8a355ab
kkavyaGoyal2003 <kkavyagoyal2008@gmail.com>
2025-10-17 22:53:05 +0530

    Time: 1 ms (44.9%), Space: 45.2 MB (34.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,33 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc] == color) return image;
         int m = image.length;
         int n = image[0].length;
 
-        int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0, 1}};
-        int oldColor = image[sr][sc];
+        int old = image[sr][sc];
+        if(old == color) return image;
 
-        dfs(image, sr, sc, color, oldColor, dir);
-
-        return image;
-    }
-    public void dfs(int[][] image, int sr, int sc,int color, int oldColor, int[][] dir ) {
-
+        int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        Queue<Integer> que = new LinkedList<>();
         image[sr][sc] = color;
-        for(int[] d : dir) {
-            int cr = d[0] + sr;
-            int cc = d[1] + sc;
-            if(cr >= 0  && cr < image.length && cc >=0 && cc < image[0].length && image[cr][cc] == oldColor) {
-                dfs(image, cr, cc, color, oldColor, dir);
+        que.add(sr);
+        que.add(sc);
+
+        while(!que.isEmpty()) {
+            int r = que.poll();
+            int c = que.poll();
+
+            for(int[] d : dir) {
+                int nr = d[0] + r;
+                int nc = d[1] + c;
+
+                if(nr >= 0  && nr < m && nc >= 0 && nc < n && image[nr][nc] == old) {
+                    image[nr][nc] = color;
+                    que.add(nr);
+                    que.add(nc);
+                }
             }
         }
+
+        return image;
     }
 }
