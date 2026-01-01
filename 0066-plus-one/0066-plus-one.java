f7a684ee3c35d6a956928a85deee459b4556b053
Kkavya Goyal <kkavyagoyal2008@gmail.com>
2026-01-01 11:42:40 +0530

    Time: 0 ms (100%), Space: 42.9 MB (96.91%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,31 +1,37 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        ArrayList<Integer> temp = new ArrayList<>();
-
         int carry = 0;
+
         for(int i = n-1; i >= 0; i--) {
             if(i == n-1) {
                 if(digits[i] < 9) {
-                    temp.add(digits[i] + 1);
+                    digits[i] += 1;
                 } else {
-                    temp.add(0);
+                    digits[i] = 0;
                     carry = 1;
                 }
             } else {
                 if(digits[i] == 9 && carry == 1) {
-                    temp.add(0);
+                    digits[i] = 0;
                 } else {
-                    temp.add(digits[i] + carry );
+                    digits[i] += carry;
                     carry = 0;
                 }
             }
             
         }
 
-        if(carry == 1) temp.add(carry);
-        Collections.reverse(temp);
-        int []ans = temp.stream().mapToInt(Integer::intValue).toArray();
+        if(carry == 0) return digits;
+
+        int[] ans = new int[n+1];
+        ans[0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            ans[i] = digits[i-1];
+        }
+
         return ans;
+
     }
 }
