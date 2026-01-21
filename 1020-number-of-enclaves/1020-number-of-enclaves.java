c7584fe0aed0f190ea2712997699157531fb8d11
Kkavya Goyal <kkavyagoyal2008@gmail.com>
2026-01-21 20:01:37 +0530

    Time: 6 ms (98.86%), Space: 61 MB (32.47%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -2,41 +2,37 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] change = new boolean[m][n];
 
         for(int i = 0; i < n; i++) {
-            if(grid[0][i] == 1 && !change[0][i]) dfs(grid, change, 0, i);
-            if(grid[m-1][i] == 1 && !change[m-1][i]) dfs(grid, change, m-1, i);
+            if(grid[0][i] == 1) dfs(grid, 0, i);
+            if(grid[m-1][i] == 1) dfs(grid, m-1, i);
         }
 
         for(int i = 1; i < m-1; i++) {
-            if(grid[i][0] == 1 && !change[i][0]) dfs(grid, change, i, 0);
-            if(grid[i][n-1] == 1 && !change[i][n-1]) dfs(grid, change, i, n-1);
+            if(grid[i][0] == 1) dfs(grid, i, 0);
+            if(grid[i][n-1] == 1) dfs(grid, i, n-1);
         }
 
         int count = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(grid[i][j] == 0 || change[i][j]) continue;
-                count++;
+                if(grid[i][j] == 1) count++;
             }
         }
 
         return count;
     }
 
-    private void dfs(int[][] grid, boolean[][] change, int r, int c) {
+    private void dfs(int[][] grid, int r, int c) {
         int m = grid.length;
         int n = grid[0].length;
-        if(change[r][c]) return;
-        change[r][c] = true;
-        int[][] dir = {{-1, 0}, { 1, 0}, {0, -1}, {0, 1}};
 
-        for(int[] d : dir) {
-            int nr = r + d[0];
-            int nc = c + d[1];
-            if(nr >= 0 && nr < m  && nc >= 0 && nc < n && grid[nr][nc] == 1) dfs(grid, change, nr, nc);
-        }
+        if(r < 0 || r >= m || c < 0 || c >=n || grid[r][c] == 0) return;
+        grid[r][c] = 0;
 
+        dfs(grid, r+1, c);
+        dfs(grid, r-1, c);
+        dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
     }
 }
