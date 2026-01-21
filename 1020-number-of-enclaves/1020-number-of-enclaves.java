a0c4571c24d8014330205ff1d8870398576cc352
Kkavya Goyal <kkavyagoyal2008@gmail.com>
2026-01-21 20:11:32 +0530

    Time: 10 ms (33.14%), Space: 53.4 MB (89.69%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -2,17 +2,48 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        Queue<Integer> que = new LinkedList<>();
 
         for(int i = 0; i < n; i++) {
-            if(grid[0][i] == 1) dfs(grid, 0, i);
-            if(grid[m-1][i] == 1) dfs(grid, m-1, i);
+            if(grid[0][i] == 1) {
+                grid[0][i] = 0;
+                que.add(0);
+                que.add(i);
+            }
+            if(grid[m-1][i] == 1) {
+                grid[m-1][i] = 0;
+                que.add(m-1);
+                que.add(i);
+            }
         }
 
         for(int i = 1; i < m-1; i++) {
-            if(grid[i][0] == 1) dfs(grid, i, 0);
-            if(grid[i][n-1] == 1) dfs(grid, i, n-1);
+            if(grid[i][0] == 1) {
+                grid[i][0] = 0;
+                que.add(i);
+                que.add(0);
+            }
+            if(grid[i][n-1] == 1) {
+                grid[i][n-1] = 0;
+                que.add(i);
+                que.add(n-1);
+            }
         }
 
+        int[][] dir = {{-1,0}, {1,0}, {0,-1}, {0, 1}};
+        while(!que.isEmpty()) {
+            int cr = que.poll();
+            int cc = que.poll();
+            for(int[] d: dir) {
+                int nr = d[0] + cr;
+                int nc = d[1] + cc;
+                if(nr >=0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 0;
+                    que.add(nr);
+                    que.add(nc);
+                }
+            }
+        }
         int count = 0;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
@@ -21,18 +52,5 @@ class Solution {
         }
 
         return count;
-    }
-
-    private void dfs(int[][] grid, int r, int c) {
-        int m = grid.length;
-        int n = grid[0].length;
-
-        if(r < 0 || r >= m || c < 0 || c >=n || grid[r][c] == 0) return;
-        grid[r][c] = 0;
-
-        dfs(grid, r+1, c);
-        dfs(grid, r-1, c);
-        dfs(grid, r, c+1);
-        dfs(grid, r, c-1);
     }
 }
