ff3aab7b6bf700fc09d9abca9f91af741ec2dd15
Kkavya Goyal <kkavyagoyal2008@gmail.com>
2026-03-12 08:50:12 +0530

    Time: 0 ms (100%), Space: 48.1 MB (68.42%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int n = nums.length;
-        if(n == 0) return new int[] {-1, -1};
+        if(n == 0) return new int[]{-1,-1};
 
-        int first = -1;
-        int last = -1;
+        int first = firstPos(nums, target);
+        int last = lastPos(nums, target);
 
+        return new int[]{first, last};
+    }
+    private int firstPos(int[] nums, int target) {
         int low = 0;
-        int high = n - 1;
-        while(low <= high) {
-            int mid = (high - low) / 2 + low;
+        int high = nums.length-1;
 
+        int ind = -1;
+        while(low <= high) {
+            int mid = low+(high-low)/2;
             if(nums[mid] == target) {
-                first = mid;
-                high = mid - 1;
-            } else if(target > nums[mid]) {
-                low = mid + 1;
+                if(ind == -1) ind = mid;
+                else ind = Math.min(ind, mid);
+            }
+            if(nums[mid] >= target) {
+                high = mid-1;
             } else {
-                high = mid - 1;
+                low = mid+1;
             }
         }
+        return ind;
+    }
+    private int lastPos(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
 
-        if(first != -1) low = first;
-        else low = 0;
-        high = n - 1;
+        int ind = -1;
         while(low <= high) {
-            int mid = (high - low) / 2 + low;
-
+            int mid = low+(high-low)/2;
             if(nums[mid] == target) {
-                last = mid;
-                low = mid + 1;
-            } else if(target > nums[mid]) {
-                low = mid + 1;
+                if(ind == -1) ind = mid;
+                else ind = Math.max(ind, mid);
+            }
+            if(nums[mid] <= target) {
+                low = mid+1;
             } else {
-                high = mid - 1;
+                high = mid-1;
             }
         }
-
-
-        return new int[] {first, last};
+        return ind;
     }
 }
